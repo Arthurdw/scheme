98b716e2bde91a90c6778a8e9a0c60e9feb1b0a8
Arthurdw <dev@arthurdw.com>
2024-10-02 14:34:00 +0200

    feat: multiply with tail and iterative recursion

--- multiply.scm
+++ multiply.scm
@@ -1,14 +1,14 @@
-; TODO: figure this out
 (define (rec-multiply a b)
-  (cond ((= b 0) 0)
-        ((= b 1) (+ (rec-multiply a 0) a))
-        ((= b 2) (+ (rec-multiply a 1) a))))
-
-(rec-multiply 5 2)
+  (if (= b 0)
+    0
+    (+ (rec-multiply a (- b 1)) a)))
 
 (define (iter-multiply a b)
   (cond ((= b 0) 0)
-        ((= b 1) (+ (rec-multiply a 0) a))
-        ((= b 2) (+ (rec-multiply a 1) a))))
+        ((= b 1) a)
+        (else (iter-multiply (+ a a) (- b 1)))))
 
 
+(rec-multiply 5 2)
+(iter-multiply 5 2)
+(iter-multiply 3 2)
