d35fce79b0a3b908e18694e59117f95610b5ceb7
Arthurdw <dev@arthurdw.com>
2024-10-16 09:13:46 +0200

    fix: forgot to save iter before commit and push

--- reverse-list.scm
+++ reverse-list.scm
@@ -3,9 +3,17 @@
     collection
     (append (rec-reverse (cdr collection)) (list (car collection)))))
 
+(define (iter-reverse collection)
+  (define (iter reversed leftover)
+    (if (null? leftover)
+      reversed
+      (iter (cons (car leftover) reversed) (cdr leftover))))
+  (iter '() collection))
+
+
 (rec-reverse '(1 2 3))
 (rec-reverse '(1))
 (rec-reverse '())
-; (iter-reverse '(1 2 3))
-; (iter-reverse '(1))
-; (iter-reverse '())
+(iter-reverse '(1 2 3))
+(iter-reverse '(1))
+(iter-reverse '())
