18ae3675032ce13c4bf4db5951f9c2b6d9dcdc5e
Arthurdw <dev@arthurdw.com>
2024-11-08 14:39:25 +0100

    doc: procedural types

---
+++ Algoritmen en datastructuren 1/h1/procedural-types.rkt
@@ -0,0 +1,12 @@
+; 1.4.1
+; cons: (any any -> pair)
+; car: (pair -> any)
+; cdr: (pair -> any)
+; vector-ref: (vector number -> vector)
+; vector-set!: (vector number any -> ∅)
+; member: (any pair -> pair U {#f})
+
+; 1.4.2
+; map: ((any -> any) vector -> vector)
+; sum: (any any (any -> boolean) (any -> any) -> vector)
+; compose: ((any -> any) (any -> any) -> (any -> any))
