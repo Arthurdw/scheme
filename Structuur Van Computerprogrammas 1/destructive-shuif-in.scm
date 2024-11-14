a974c4b0ef2566f0160c86a4d143752ee7bc68c2
Arthurdw <dev@arthurdw.com>
2024-11-14 14:35:25 +0100

    feat: proper shuif-in without new cons allocations

--- Structuur Van Computerprogrammas 1/destructive-shuif-in.scm
+++ Structuur Van Computerprogrammas 1/destructive-shuif-in.scm
@@ -1,14 +1,26 @@
+; This does not suffice to the task requirements :(
+; (define (schuif-in! lst1 lst2)
+;   (if (null? (cdr lst1))
+;     (begin
+;       (set-cdr! lst1 lst2)
+;       'ok)
+;     (begin
+;       (set-cdr! lst1 (cons (car lst2) (cdr lst1)))
+;       (schuif-in! (cddr lst1) (cdr lst2)))))
+
 (define (schuif-in! lst1 lst2)
   (if (null? (cdr lst1))
     (begin
       (set-cdr! lst1 lst2)
       'ok)
-    (begin
-      (set-cdr! lst1 (cons (car lst2) (cdr lst1)))
-      (schuif-in! (cddr lst1) (cdr lst2)))))
+    (let ((after1 (cdr lst1))
+          (after2 (cdr lst2)))
+      (set-cdr! lst1 lst2)
+      (set-cdr! (cdr lst1) after1)
+      (schuif-in! after1 after2))))
 
 
 (define lijst1 '(1 3 5))
 (define lijst2 '(2 4 6 8))
 (schuif-in! lijst1 lijst2) ; ; (out) ok
-lijst1 ; ; (out) (1 2 2 4 3 6 4 8 5 6 8)
+lijst1  ; ; (out) (1 2 3 4 5 6 8)
